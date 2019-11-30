32d965aa16518632e0e4d25d875af595cf7294ec
carlo <cetoli@gmail.com>
2019-11-29 21:57:15 -0300

    Algumas dimensões do cubo

--- src/hash3d.py
+++ src/hash3d.py
@@ -2,26 +2,47 @@ from time import sleep
 import copy
 import serial
 
-leitura = serial.Serial('COM7', 9600)
+leitura = object()  # serial.Serial('COM7', 9600)
+
 
 class Tabuleiro:
     def __init__(self):
-        self.casa = [[[0,0,0]for y in range(3)]for z in range(3)]
+        self.casa = [[[0, 0, 0]for _ in range(3)]for __ in range(3)]
         self.valor = ""
-        self.peca_cor = copy.deepcopy(self.casa)
-        self.peca_cor = [[[(int(peca)-1)//5+1 for peca in linha] for linha in nivel] for nivel in self.peca_cor]
+        self.peca_cor = self.peca_dot = self.casa
+        self.atualiza()
 
-    def atualiza(self, arduino = [0]*27):
+    def atualiza(self, arduino=(0,)*27):
         arduino = [arduino[0:9], arduino[9:18], arduino[18:27]]
         self.casa = [[linha[0:3], linha[3:6], linha[6:9]] for linha in arduino]
         self.peca_cor = copy.deepcopy(self.casa)
-        self.peca_cor = [[[(int(peca) - 1) // 5+1 for peca in linha] for linha in nivel] for nivel in self.peca_cor]
-        velha = self.velha(self.peca_cor)
-        print ("Número de acertos", velha)
-        return velha
+        self.peca_dot = copy.deepcopy(self.casa)
+        self.peca_cor = [[[(int(peca)-1)//5+1 for peca in linha] for linha in nivel] for nivel in self.peca_cor]
+        self.peca_dot = [[[(int(peca)-1) % 5+1 if int(peca) else 0 for peca in linha]
+                          for linha in nivel] for nivel in self.peca_dot]
+        return self.pontua(self.peca_cor) + self.pontua(self.peca_dot)
 
-    def velha(self, tabuleiro):
-        return sum([1 if (len(set(linha)) == 1 and 0 not in linha) else 0 for nivel in tabuleiro for linha in nivel])
+    def pontua(self, cubo):
+        pontos = self.crivo(cubo)
+        # [print(nivel) for nivel in cubo]
+        linhas_y = [zip(*nivel) for nivel in cubo]
+        # [print([[x for x in y] for y in nivel]) for nivel in linhas_y]
+        pontos += self.crivo(linhas_y)
+        linhas_z = [[[casa for casa in linha] for linha in nivel] for nivel in zip(*cubo)]
+        # [print([[x for x in y] for y in nivel]) for nivel in linhas_z]
+        pontos += self.crivo(linhas_z)
+        colunas_z = [[[casa for casa in linha] for linha in zip(*nivel)] for nivel in zip(*cubo)]
+        [print([[x for x in y] for y in nivel]) for nivel in colunas_z]
+        pontos += self.crivo(colunas_z)
+        linhas_x = [zip(*[[casa for casa in linha] for linha in nivel]) for nivel in cubo]
+        pontos += self.crivo(linhas_x)
+
+        print("Número de acertos", pontos)
+        return pontos
+
+    @staticmethod
+    def crivo(tabuleiro_):
+        return sum([1 if (len(set(linha)) == 1 and 0 not in linha) else 0 for nivel in tabuleiro_ for linha in nivel])
 
     def leitor(self):
         lido = leitura.readline().decode("utf8") + ":"
@@ -33,25 +54,34 @@ class Tabuleiro:
             atualizador = tripa[0].split()
             self.valor = ""
             print(atualizador)
-            tabul.atualiza(atualizador)
-            # print("tabuleiro",tabul.casa)
-            [print(linha) for nivel in tabul.casa for linha in nivel]
+            tabuleiro.atualiza(atualizador)
+            # print("tabuleiro",tabuleiro.casa)
+            [print(linha) for nivel in tabuleiro.casa for linha in nivel]
 
 
 def main():
-    tabuleiro = Tabuleiro()
-    return tabuleiro
+    tabuleiro_ = Tabuleiro()
+    return tabuleiro_
 
 
-if __name__ == '__main__' :
-    tabul = main()
-    #velha = tabul.atualiza(list(range(1,28)))
-    #print ("Numero de velhas", velha)
-    #[print(nivel) for nivel in tabul.peca_cor]
-    #assert tabul.casa[0][0] == [1,0,0], f" mas era {tabul.casa[0][0]}"
-    #tabul.atualiza([2]+[0]*26)
-    #assert tabul.casa[0][0] == [2, 0, 0], f" mas era {tabul.casa[0][0]}"
+if __name__ == '__main__':
+    tabuleiro = main()
+    velha = tabuleiro.atualiza(list(range(1, 28)))
+    print("Numero de velhas", velha)
 
-    for i in range(30):
-        tabul.leitor()
+    '''verticais_ = [zip(*nivel) for nivel in tabuleiro.peca_cor]
+    verticais_ = [[[casa for casa in linha] for linha in nivel]for nivel in verticais_]
+    altitudes = [[[casa for casa in linha] for linha in nivel]for nivel in zip(*tabuleiro.peca_cor)]
+    azimutes = [[[casa for casa in linha] for linha in zip(*nivel)]for nivel in zip(*tabuleiro.peca_cor)]
+
+    [print(nivel) for nivel in tabuleiro.peca_cor]
+    [print(nivel) for nivel in altitudes]
+    [print(nivel) for nivel in azimutes]'''
+    # [print(nivel) for nivel in verticais_]
+    # assert tabuleiro.casa[0][0] == [1,0,0], f" mas era {tabuleiro.casa[0][0]}"
+    # tabuleiro.atualiza([2]+[0]*26)
+    # assert tabuleiro.casa[0][0] == [2, 0, 0], f" mas era {tabuleiro.casa[0][0]}"
+
+    for i in range(0):
+        tabuleiro.leitor()
         sleep(1)
